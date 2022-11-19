4266dad7050b7b516e076d5d028ce054792a65a4
fuadhasan08 <68866471+fuadhasan08@users.noreply.github.com>
2022-11-19 14:58:16 +0600

    1 file modified

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,17 @@ function App() {
   return (
     <>
       <h1>Hello AS</h1>
+      <div className="one">
+        <li className="myList">
+          <a href="#">One</a>
+        </li>
+        <li className="myList">
+          <a href="#">Two</a>
+        </li>
+        <li className="myList">
+          <a href="#">Three</a>
+        </li>
+      </div>
     </>
   );
 }
